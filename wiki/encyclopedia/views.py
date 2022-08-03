49b4fa7fe2af79d483b1bb6d7e5fa40b599a5271
unknown <marcan.grab@gmail.com>
2022-08-03 15:37:06 +0200

    creating new page

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -4,9 +4,16 @@ from django.shortcuts import render
 
 from . import util
 
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
+
+from django.urls import reverse
 
 class NewPageForm(forms.Form):
+    new_page_title = forms.CharField(label="New Page title")
+    new_page_text = forms.CharField(widget=forms.Textarea)
+
+    
+
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -37,7 +44,20 @@ def query(request):
             "entries": entries
         })
 def new_page(request):
-    return render(request, "encyclopedia/new_page.html", {
+    if request.method == "POST":
+        form = NewPageForm(request.POST)
+        if form.is_valid():
+            new_page_title = form.cleaned_data["new_page_title"]
+            print(type(new_page_title))
+            new_page_text = form.cleaned_data["new_page_text"]
+            util.save_entry(new_page_title, new_page_text)
+            return HttpResponseRedirect(reverse("wiki:entry", args= f"{new_page_title}"))
 
+        else:
+            return render(request, "encyclopedia/new_page.html",{
+                "form": form
+            })
+    return render(request, "encyclopedia/new_page.html", {
+        "form":NewPageForm() 
     })
                
